e86c446d2e79b1bee062fcf0cdc24e5d9f520b87
Novvator <stamatis.sellas@yahoo.com>
2020-05-02 22:20:27 +0300

    Init Test burgers added

---
+++ im_hungry/Assets/ColliderDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        while (Input.touchCount > i)
+        {
+            Touch touch = Input.GetTouch(i);
+            Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+            if (hit != null && hit.collider != null)
+            {
+                Debug.Log("I'm hitting " + hit.collider.name);
+                Destroy(hit.collider.gameObject);
+            
+            }
+
+            ++i;
+        }
+    }
+}
---
+++ im_hungry/Assets/Disappear.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        while (Input.touchCount > i)
+        {
+            Touch touch = Input.GetTouch(i);
+            if (touch.phase == TouchPhase.Began)
+            {
+               // Debug.Log("touch began");
+                //Destroy(gameObject);
+                
+            }
+            ++i;
+
+        }
+    }
+ }
