8eb296e9e84abb2c29737f05d1f2263c44f62c9b
Novvator <stamatis.sellas@yahoo.com>
2020-05-03 15:45:33 +0300

    Added reload button for testing

--- im_hungry/Assets/ColliderDetector.cs
+++ im_hungry/Assets/ColliderDetector.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ColliderDetector : MonoBehaviour
 {
+    public GameObject[] objects;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,20 +14,31 @@ public class ColliderDetector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int i = 0;
-        while (Input.touchCount > i)
+
+        if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(i);
+            Touch touch = Input.GetTouch(0);
             Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
             RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
             if (hit != null && hit.collider != null)
             {
                 Debug.Log("I'm hitting " + hit.collider.name);
-                Destroy(hit.collider.gameObject);
+                if(hit.collider.name == "reload")
+                {
+                    objects[0].SetActive(true);
+                    objects[1].SetActive(true);
+                    objects[2].SetActive(true);
+                }
+
+                else
+                {
+
+                    hit.collider.gameObject.SetActive(false);
+
+                }
+                
             
             }
-
-            ++i;
         }
     }
 }
