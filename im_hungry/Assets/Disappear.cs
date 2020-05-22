4c067b4a13fb0a78b22fda17ae9b85a0e2d0e2ef
Novvator <stamatis.sellas@yahoo.com>
2020-05-22 17:01:36 +0300

    testing pattern recognition

--- im_hungry/Assets/Disappear.cs
+++ im_hungry/Assets/Disappear.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Disappear : MonoBehaviour
-
-    
 {
+    public GameObject[] objectss;
     // Start is called before the first frame update
     void Start()
-    { 
-        
+    {
+        objectss[4].SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(objectss[0].activeSelf == false && objectss[1].activeSelf == false 
+            && objectss[2].activeSelf == false)
+        {
+            objectss[3].SetActive(false);
+            objectss[4].SetActive(true);
+        }
+        
     }
  }
 
---
+++ im_hungry/Assets/PatternScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternScript : MonoBehaviour
+{
+    public GameObject[] poz;
+    int endd = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        poz[1].SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (var i = 0; i < Input.touchCount; ++i)
+        {
+            Touch touch = Input.GetTouch(i);
+            Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+            if (hit != null && hit.collider != null)
+            {
+                Debug.Log("I'm hitting " + hit.collider.name);
+                
+                    while (touch.phase == TouchPhase.Moved)
+                    {
+                        while (hit.collider.name == "tubee")
+                        {
+                            if (hit.collider.name == "finish")
+                            {
+                                endd = 1;
+                            }
+                            if (endd == 1) { break; }
+
+                        }
+                        if (endd == 1) { break; }
+                    }
+                    if (endd == 1) { poz[1].SetActive(true); }
+               
+
+
+            }
+        }
+    }
+}
