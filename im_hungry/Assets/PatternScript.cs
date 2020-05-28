5fb8eb6e7b795613d99aabc572bd9fafe158d660
Novvator <stamatis.sellas@yahoo.com>
2020-05-28 18:59:35 +0300

    Added reset when removing touch Changing scene when completing pattern

--- im_hungry/Assets/PatternScript.cs
+++ im_hungry/Assets/PatternScript.cs
@@ -8,7 +8,9 @@ public class PatternScript : MonoBehaviour
 {
     public GameObject[] burgers;
     public GameObject[] stage;
-    int hitted = 0;
+    bool hitted = false;
+    bool trg1 = false;
+    bool trg2 = false;
 
     // Start is called before the first frame update
     void Start()
@@ -26,46 +28,67 @@ public class PatternScript : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
             RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
-            if (hit != null && hit.collider != null)
+            if (hit != null && hit.collider != null && touch.phase != TouchPhase.Ended)
             {
                 
                 Debug.Log("I'm hitting " + hit.collider.name);
 
+                
+
                 if (hit.collider.name == "tubee")
                 {
-                    hitted = 1;
+                    hitted = true;
                 }
 
-                if(hit.collider.name == "2" && hitted == 1)
+                if(hit.collider.name == "2" && hitted)
                 {
                     burgers[0].SetActive(false);
                     burgers[1].SetActive(true);
-
+                    burgers[2].SetActive(false);
+                    trg1 = true;
                 }
                 
-                if(hit.collider.name == "3" && hitted == 1)
+                if(hit.collider.name == "3" && hitted && trg1)
                 {
+                    burgers[0].SetActive(false);
                     burgers[1].SetActive(false);
                     burgers[2].SetActive(true);
+                    trg2 = true;
 
                 }
-                if (hit.collider.name == "finish" && hitted == 1)
+                if (hit.collider.name == "finish" && hitted && trg1 & trg2)
                 {
+                    burgers[0].SetActive(false);
+                    burgers[1].SetActive(false);
                     burgers[2].SetActive(false);
+                    SceneManager.LoadScene("scene2");
                 }
 
             }
             else
             {
-                if (hitted == 1)
+                if (hitted == true)
                 {
                     Debug.Log("Lost target");
-                    hitted = 0;
+                    hitted = false;
 
                     //when leaving tube to reset first burger
-                    //burgers[0].SetActive(true);
-                    //burgers[1].SetActive(false);
-                    //burgers[2].SetActive(false);
+                    if(burgers[0].activeSelf == false)
+                    {
+                        burgers[0].SetActive(true);
+                    }
+
+                    if (burgers[1].activeSelf == true)
+                    {
+                        burgers[1].SetActive(false);
+                    }
+
+                    if (burgers[2].activeSelf == true)
+                    {
+                        burgers[2].SetActive(false);
+                    }
+                    
+                  
                 }
             }
 
