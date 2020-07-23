01de58b91b0527307091a5ea51b44be20c30191b
Novvator <stamatis.sellas@yahoo.com>
2020-07-23 16:56:45 +0300

    Added moving animation when food appears

---
+++ im_hungry/Assets/MoveToStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToStart : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject target;
+    float Wradius = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
+    }
+}
