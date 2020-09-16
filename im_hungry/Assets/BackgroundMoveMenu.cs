2f654735bfaa13f823f9592dfa657596f00a2fbf
Novvator <stamatis.sellas@yahoo.com>
2020-09-16 22:38:10 +0300

    Added main menu Changed pattern components

---
+++ im_hungry/Assets/BackgroundMoveMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMoveMenu : MonoBehaviour
+{
+	public float speed, clamppos;
+	Vector3 startpos;
+
+	// Use this for initialization
+	void Start()
+	{
+		startpos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float newpos = Mathf.Repeat(Time.time * speed, clamppos);
+		transform.position = startpos + Vector3.left * newpos + Vector3.up * newpos;
+	}
+}
