cc1c2f6cb48c336183dd0d68800b4f923f8b1528
DESKTOP-E87RC1R\SenpaiOrigin <stamatis.sellas@yahoo.com>
2021-01-28 21:49:25 +0200

    added menu button

---
+++ im_hungry/Assets/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("pattern scene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
