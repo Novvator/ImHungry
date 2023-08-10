41aa1c2e7a20835bad5dc441bfbbda3654b01d8a
Novvator <stamatis.sellas@yahoo.com>
2023-08-10 18:46:28 +0300

    added latest changes

---
+++ im_hungry/Assets/LevelChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeToLevel ()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeOutComplete()
+    {
+        SceneManager.LoadScene(1);
+        animator.SetTrigger("FadeIn");
+
+    }
+}
