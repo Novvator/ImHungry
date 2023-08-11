bd9b28d12589d5e93bbd9f8c22e0d5668444124c
Novvator <stamatis.sellas@yahoo.com>
2023-08-11 19:37:34 +0300

    latest changes

---
+++ im_hungry/Assets/Pattern.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Pattern : MonoBehaviour
+{
+    public Transform[] checkpoints;  // Define checkpoints in the Unity Inspector
+    public Transform destination;   // Define destination point in the Unity Inspector
+    public float timeLimit = 30f;   // Time limit for completing the pattern
+
+    private bool isPatternCompleted;
+    private bool isPatternPart1Hit;
+    private bool isPatternPart2Hit;
+    
+    public void ResetPattern()
+    {
+        isPatternCompleted = false;
+        isPatternPart1Hit = false;
+        isPatternPart2Hit = false;
+
+        // Reset pattern visuals and animations here
+    }
+
+    public void UpdatePatternInteraction()
+    {
+        if (!isPatternCompleted)
+        {
+            // Update pattern interaction logic based on touch input
+        }
+    }
+
+    // Add other methods for handling pattern interactions, animations, and more
+
+    private bool CheckIfPatternIsCompleted()
+    {
+        // Check if the user has successfully completed the pattern based on checkpoint interactions
+        // and reaching the destination point
+        return isPatternPart1Hit && isPatternPart2Hit && IsTouchingDestination();
+    }
+
+    private bool IsTouchingDestination()
+    {
+        // Implement logic to check if the user's touch is within a certain threshold of the destination point
+        // This can involve distance calculations between the touch position and the destination point
+        return false;
+    }
+}
