59525cad3b79420e2dc715c5949e2eb452511c29
Novvator <stamatis.sellas@yahoo.com>
2024-07-06 12:11:11 +0300

    added semi working worlds

--- im_hungry/Assets/LevelChanger.cs
+++ im_hungry/Assets/LevelChanger.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +11,9 @@ public class LevelChanger : MonoBehaviour
 
     public Animator animator;
     public float transitionTime = 1f;
+    private int world1Levels = 3;
+    private int world2Levels = 3;
+    private int world3Levels = 3;
 
     private void Awake()
     {
@@ -25,8 +31,22 @@ public class LevelChanger : MonoBehaviour
     {
         Time.timeScale = 1f; // Resume normal time scale
         if (level == "current")
-        {   
+        {
             StartCoroutine(LoadLevelCoroutine(SceneManager.GetActiveScene().name));
+        }
+        else if (level == "Level Menu")
+        {
+            if (SceneManager.GetActiveScene().name.Contains("Stage"))
+            {
+                string subjectString = SceneManager.GetActiveScene().name;
+                int resultInt = Int32.Parse(Regex.Match(subjectString, @"\d+").Value); //extract stage number
+                string level_to_load = "Level Menu" + LevelMenuLoad(resultInt);
+                StartCoroutine(LoadLevelCoroutine(level_to_load));
+            }
+            else
+            {
+                StartCoroutine(LoadLevelCoroutine(level));
+            }
         }
         else
         {
@@ -45,9 +65,24 @@ public class LevelChanger : MonoBehaviour
 
     }
 
+    private string LevelMenuLoad(int level)
+    {
+        if (level <= world1Levels)
+        {
+            return "";
+        }
+        else if (world1Levels < level && level <= world1Levels + world2Levels)
+        {
+            return "2";
+        }
+        else
+        {
+            return "3";
+        }
 
 
-    
 
-    
+
+
+    }
 }
